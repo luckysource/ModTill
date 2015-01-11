f81db5e4a4ffe139f98181043269a72dbcd9a934
luckysource <alecgray@gmail.com>
2015-01-11 00:17:36 +0000

    Added proxies for common/client/server Added Reference class Added mcmod.info file Added configuration handling

---
+++ src/main/java/com/lucky/ModTill/ModTill.java
@@ -0,0 +1,38 @@
+package com.lucky.ModTill;
+
+import configuration.ConfigurationHandler;
+import proxy.IProxy;
+import reference.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid= Reference.MOD_ID, name = Reference.MOD_NAME, version= Reference.VERSION)
+public class ModTill {
+	
+	@Mod.Instance(Reference.MOD_ID)
+	public static ModTill instance;
+	 
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+	public static IProxy proxy;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		//network handling
+		//mod config - user can tweak settings
+		//item and block initialize
+		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+	}
+	
+	@Mod.EventHandler
+	public void preInit(FMLInitializationEvent event){
+		//register GUI, tile entities, crafting recipes
+	}
+	
+	@Mod.EventHandler
+	public void preInit(FMLPostInitializationEvent event){
+		//wrap anything up after other mods have initialized
+	}
+}
---
+++ src/main/java/reference/Reference.java
@@ -0,0 +1,9 @@
+package reference;
+
+public class Reference {
+	public static final String MOD_ID = "ModTill";
+	public static final String MOD_NAME = "Modified Tilling";
+	public static final String VERSION = "1.7.10-1.0";
+	public static final String CLIENT_PROXY_CLASS = "com.lucky.ModTill.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "com.lucky.ModTill.proxy.ServerProxy";
+}
